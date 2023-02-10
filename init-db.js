f0f3b697ad0365c54f993e01650415104b5b95c7
Sil <pescadorlopezs@gmail.com>
2023-02-10 21:38:10 +0100

    add user _id to default users in init-db

--- init-db.js
+++ init-db.js
@@ -1,16 +1,14 @@
-const jwt = require('jsonwebtoken')
-
 // Inicializar la base datos con los datos mínimos para funcionar
 
-
 const readline = require('readline');
-
 
 // cargamos los modelos
 
 const Tweet = require('./models/tweet');
 const User = require('./models/user');
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const ObjectId = mongoose.Types.ObjectId
 
 
 
@@ -40,42 +38,55 @@ main().catch(err => console.log('Error', err));
 let users = []
 
 async function initUsers() {
-
+  
   // delete all users in database
   const resultUsers = await User.deleteMany({});
   console.log(`Deleted ${resultUsers.deletedCount} users.`)
   
   // create 5 new users
   const user1 = new User ({
+    _id: new ObjectId(),
     username: 'sil',
     email: 'pescadorlopezsilvia@gmail.com',
     password: '123456',
-    jwtInfo: jwt.sign({ id: user1._id, email: user1.email }, 'flitter', { expiresIn: '1h' })
   });
+
+  user1.jwtInfo = jwt.sign({ id: user1._id, email: user1.email }, 'flitter')
+
   const user2 = new User ({
+    _id: new ObjectId(),
     username: 'mari',
     email: 'xixiadecosta@gmail.com',
     password: '123456',
-    jwtInfo: jwt.sign({ id: user2._id, email: user2.email }, 'flitter', { expiresIn: '1h' })
   });
+  user2.jwtInfo = jwt.sign({ id: user2._id, email: user2.email }, 'flitter')
+
   const user3 = new User ({
+    _id: new ObjectId(),
     username: 'mollete',
     email: 'pescadorlopezs@gmail.com',
     password: '123456',
-    jwtInfo: jwt.sign({ id: user3._id, email: user3.email }, 'flitter', { expiresIn: '1h' })
   });
+  
+  user3.jwtInfo = jwt.sign({ id: user3._id, email: user3.email }, 'flitter')
+
   const user4 = new User ({
+    _id: new ObjectId(),
     username: 'muki',
     email: 'silvikini@hotmail.com',
     password: '123456',
-    jwtInfo: jwt.sign({ id: user4._id, email: user4.email }, 'flitter', { expiresIn: '1h' })
   });
+  
+  user4.jwtInfo = jwt.sign({ id: user4._id, email: user4.email }, 'flitter')
+
   const user5 = new User ({
+    _id: new ObjectId(),
     username: 'neli',
     email: 'maantoana@gmail.com',
     password: '123456',
-    jwtInfo: jwt.sign({ id: user5._id, email: user5.email }, 'flitter', { expiresIn: '1h' })
   });
+  
+  user5.jwtInfo = jwt.sign({ id: user5._id, email: user5.email }, 'flitter')
   
   // save new users in database
   user1.save();
